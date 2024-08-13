b112def53c5f82a3deb641abb09d3abc715fa4e2
emanoelle9 <emanoelleferreiramateus@gmail.com>
2024-08-13 09:31:50 -0300

    fix: correção das consultas de get e update

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -25,7 +25,11 @@ getProductByBarcode = async (req, res) =>{
     try{
         const { barcode } = req.params
 
-        const product = await Product.findOne ({ barcode })
+        const product = await Product.findOne ({ 
+            where:{
+                barcode: barcode
+        }
+        })
 
         if (!product){
             return res.status(400).json({ message: "Produto não encontrado"})
@@ -43,7 +47,11 @@ updateProductByBarcode = async (req, res) =>{
         const { barcode } = req.params
         const {name, price, description, stock} = req.body
 
-        const product = await Product.findOne({barcode})
+        const product = await Product.findOne({
+            where:{
+                barcode: barcode
+        }
+        })
 
         if (!product){
             return res.status(400).json({error: 'Produto não encontrado'})
